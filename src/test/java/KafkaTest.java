1bf2117f71cd04ba3778e5992cf7282988c339dc
WildMrZhang <lang1422310418@hotmail.com>
2019-12-25 22:04:02 +0800

    兼容时间表达式容错

--- src/test/java/KafkaTest.java
+++ src/test/java/KafkaTest.java
@@ -1,7 +1,15 @@
 import com.ailbb.act.$;
+import com.ailbb.act.entity.$ConfSite;
 import com.ailbb.act.entity.$KafkaConnConfiguration;
+import com.ailbb.act.entity.$KerberosConnConfiguration;
+import com.ailbb.act.hadoop.$Hadoop;
 import com.ailbb.act.kafka.$Kafka;
+import com.ailbb.act.kerberos.$Kerberos;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.security.PrivilegedExceptionAction;
 
 /**
  * Created by Wz on 9/10/2018.
@@ -9,10 +17,39 @@ import org.junit.Test;
 public class KafkaTest {
     @Test
     public void test() throws Exception {
-//        Assert.assertEquals("", KafkaTest.init());
+//        initKerberos();
+//        initKafka();
+
+//        $.kafka.send("topic", "hello");
     }
 
-    public static $Kafka init() throws Exception {
+    public static $Kerberos initKerberos() {
+        $KerberosConnConfiguration kerberosConnConfiguration = new $KerberosConnConfiguration();
+        $ConfSite confSite = new $ConfSite();
+
+        kerberosConnConfiguration // berberos所在机器
+                .setPrincipal("hive/db@CLOUDERA")
+                .setKeyTab("/keytab/hive_db.keytab")
+
+                .setIp("127.0.0.1")
+                .setUsername("root")
+                .setPassword("123456")
+        ;
+
+        confSite.setCoreSite("/broadtech/core-site.xml")
+                .setHiveSite("/broadtech/hive-site.xml")
+                .setHdfsSite("/broadtech/hdfs-site.xml")
+                .setHbaseSite("/broadtech/hbase-site.xml")
+                .setSentrySite("/broadtech/sentry-site.xml")
+                .setYarnSite("/broadtech/yarn-site.xml")
+                .setMapredSite("/broadtech/mapred-site.xml")
+                .setSslClientSite("/broadtech/ssl-client-site.xml");
+
+        // 初始化
+        return $.kerberos.init(kerberosConnConfiguration, confSite);
+    }
+
+    public static $Kafka initKafka() throws Exception {
         $KafkaConnConfiguration kafkaConnConfiguration = new $KafkaConnConfiguration();
 
         kafkaConnConfiguration.setIp("slave08");
--- src/test/java/KerBerosTest.java
+++ src/test/java/KerBerosTest.java
@@ -1,41 +1,57 @@
-import com.ailbb.act.$;
-import com.ailbb.act.entity.$ConfSite;
-import com.ailbb.act.entity.$KerberosConnConfiguration;
 import com.ailbb.act.kerberos.$Kerberos;
-import com.ailbb.ajj.entity.$JDBCConnConfiguration;
-import org.junit.Assert;
-import org.junit.Test;
+import org.apache.hadoop.fs.FileStatus;
 
 /**
  * Created by Wz on 9/10/2018.
  */
 public class KerBerosTest {
-    @Test
-    public void test() throws Exception {
-//        Assert.assertEquals("", KerBerosTest.init());
+    public static void doTest() throws Exception {
+        java.lang.System.setProperty("java.security.krb5.conf",  "/etc/krb5.conf");
+        org.apache.hadoop.conf.Configuration hconf = new org.apache.hadoop.conf.Configuration();
+        hconf.set("hadoop.security.authentication", "kerberos");
+        hconf.addResource(new org.apache.hadoop.fs.Path("/etc/hadoop/mapred-site.xml"));
+        hconf.addResource(new org.apache.hadoop.fs.Path("/etc/hadoopyarn-site.xml"));
+        hconf.addResource(new org.apache.hadoop.fs.Path("/etc/hadoop/conf/core-site.xml"));
+        hconf.addResource(new org.apache.hadoop.fs.Path("/etc/hadoop/conf/hdfs-site.xml"));
+        hconf.addResource(new org.apache.hadoop.fs.Path("/etc/hbase/conf/hbase-site.xml"));
+        hconf.addResource(new org.apache.hadoop.fs.Path("/etc/hadoop/conf/yarn-site.xml"));
+        hconf.addResource(new org.apache.hadoop.fs.Path("/etc/hive/conf/hive-site.xml"));
+
+        org.apache.hadoop.security.UserGroupInformation.setConfiguration(hconf);
+        org.apache.hadoop.security.UserGroupInformation.loginUserFromKeytabAndReturnUGI("hive/db", "/broadtech/share/keytab/hive_db.keytab")
+                .doAs(new java.security.PrivilegedExceptionAction() {
+                    @Override
+                    public Object run() throws Exception {
+
+                            System.out.println("=========Kerberos验证成功，开始打印HDFS目录========== ");
+
+                            FileStatus[] x = org.apache.hadoop.fs.FileSystem.newInstance(hconf).listStatus(new org.apache.hadoop.fs.Path("/"));
+                            for(int i=0;i<x.length;i++)
+                                System.out.println(x[i].getPath());
+
+                            System.out.println("=========开始执行impala刷新========== ");
+
+                            java.lang.Class.forName("com.cloudera.impala.jdbc41.Driver");
+                            java.sql.Connection conn = java.sql.DriverManager.getConnection("jdbc:impala://192.168.2.138:21050/default;AuthMech=1;KrbHostFQDN=slave31;KrbServiceName=impala", "", "");
+                            java.sql.Statement stat = conn.createStatement();
+
+                            try {
+                                stat.executeUpdate("refresh prestat_test.st_app_hour ");
+                                System.out.println("成功: ");
+                            } finally {
+                                // db.close();
+                                stat.close();
+                                conn.close();
+                            }
+
+
+                        return null;
+                    }
+                });
+
     }
 
-    public static $Kerberos init(){
-        $KerberosConnConfiguration kerberosConnConfiguration = new $KerberosConnConfiguration();
-        $ConfSite confSite = new $ConfSite();
-
-        kerberosConnConfiguration // berberos所在机器
-                .setPrincipal("hive/db@CLOUDERA")
-                .setKeyTab("/home/keytab/hive_db.keytab")
-
-                .setIp("127.0.0.1")
-                .setUsername("root")
-                .setPassword("123456")
-        ;
-
-        confSite.setCoreSite("/home/core-site.xml")
-                .setHbaseSite("/home/hbase-site.xml")
-                .setHdfsSite("/home/hdfs-site.xml")
-                .setHiveSite("/home/hive-site.xml")
-                .setMapredSite("/home/mapred-site.xml")
-                .setYarnSite("/home/yarn-site.xml");
-
-        // 初始化
-        return $.kerberos.init(kerberosConnConfiguration, confSite);
+    static $Kerberos init(){
+        return null;
     }
 }
